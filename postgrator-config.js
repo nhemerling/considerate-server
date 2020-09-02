1d2e9fab8283b4dd04489751d6515f70579843a9
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-01 20:46:57 -0700

    add get friends endpoint

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,7 +6,5 @@ module.exports = {
   connectionString:
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DATABASE_URL
-      : process.env.NODE_ENV === 'development'
-      ? process.env.DEV_DATABASE_URL
       : process.env.DATABASE_URL,
 };
