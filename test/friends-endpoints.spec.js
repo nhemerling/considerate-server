bedcf2a5a3cb7ae66133a623b702fa90d43f1fcb
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-09 14:29:58 -0700

    added tests for all friends endpoints

--- test/friends-endpoints.spec.js
+++ test/friends-endpoints.spec.js
@@ -2,11 +2,19 @@ const knex = require('knex');
 const bcrypt = require('bcryptjs');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
+const supertest = require('supertest');
+const { expect } = require('chai');
+const { makeUsersArray } = require('./test-helpers');
 
-describe('Friends Endpoints', function () {
+describe.only('Friends Endpoints', function () {
   let db;
 
-  const { testUsers, testFriends } = helpers.makeFriendsFixtures();
+  const {
+    testUsers,
+    testFriends,
+    testLikes,
+    testFriendLikes,
+  } = helpers.makeFriendsFixtures();
 
   before('make knex instance', () => {
     db = knex({
@@ -22,5 +30,417 @@ describe('Friends Endpoints', function () {
 
   afterEach('cleanup', () => helpers.cleanTables(db));
 
-  describe('GET /api/friends', () => {});
+  describe('GET /api/friends', () => {
+    context('Given no friends', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+      it('Responds with 200 and an empty list', () => {
+        return supertest(app)
+          .get('/api/friends')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200, []);
+      });
+    });
+
+    context('Given there are friends in the database', () => {
+      beforeEach('insert friends', () =>
+        helpers.seedFriendsTables(
+          db,
+          testUsers,
+          testFriends,
+          testLikes,
+          testFriendLikes
+        )
+      );
+
+      it('responds with 200 and all of the friends', () => {
+        const testUser = testUsers[0];
+        const testUserFriends = testFriends.filter(
+          (friend) => friend.user_id === testUser.id
+        );
+        const expectedFriends = testUserFriends.map((friend) =>
+          helpers.makeExpectedFriend(friend)
+        );
+
+        return supertest(app)
+          .get('/api/friends')
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .expect(200, expectedFriends);
+      });
+    });
+
+    context('Given an XSS attack friend', () => {
+      const testUser = helpers.makeUsersArray()[1];
+      const { maliciousFriend, expectedFriend } = helpers.makeMaliciousFriend(
+        testUser
+      );
+
+      beforeEach('insert malicious friend', () =>
+        helpers.seedMaliciousFriend(db, testUser, maliciousFriend)
+      );
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get('/api/friends')
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .expect(200)
+          .expect((res) => {
+            expect(res.body[0].friend_name).to.eql(expectedFriend.friend_name);
+            expect(res.body[0].occasion).to.eql(expectedFriend.occasion);
+          });
+      });
+    });
+  });
+
+  describe('GET /api/friends/:friend_id', () => {
+    context('Given no friends', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+
+      it('Responds with 404', () => {
+        const friendId = 12345;
+        return supertest(app)
+          .get(`/api/friends/${friendId}`)
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(404, { error: `Friend doesn't exist` });
+      });
+    });
+
+    context('Given there are friends in the database', () => {
+      beforeEach('insert friends', () =>
+        helpers.seedFriendsTables(
+          db,
+          testUsers,
+          testFriends,
+          testLikes,
+          testFriendLikes
+        )
+      );
+
+      it('Responds with 200 and the specific friend', () => {
+        const friendId = 1;
+        const testUser = testUsers[0];
+        const userFriend = testFriends.filter(
+          (friend) => friend.id === friendId
+        )[0];
+        const expectedFriend = helpers.makeExpectedFriend(userFriend);
+
+        return supertest(app)
+          .get(`/api/friends/${friendId}`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .expect(200, expectedFriend);
+      });
+    });
+
+    context('Given an XSS attack friend', () => {
+      const testUser = helpers.makeUsersArray()[1];
+      const { maliciousFriend, expectedFriend } = helpers.makeMaliciousFriend(
+        testUser
+      );
+
+      beforeEach('insert malicious friend', () =>
+        helpers.seedMaliciousFriend(db, testUser, maliciousFriend)
+      );
+
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/api/friends/${maliciousFriend.id}`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.friend_name).to.eql(expectedFriend.friend_name);
+            expect(res.body.occasion).to.eql(expectedFriend.occasion);
+          });
+      });
+    });
+  });
+
+  describe('GET /api/friends/:friend_id/likes', () => {
+    context('Given no likes', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+
+      it('Responds with 404', () => {
+        const friendId = 12345;
+        return supertest(app)
+          .get(`/api/friends/${friendId}/likes`)
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(404, { error: `Friend doesn't exist` });
+      });
+    });
+
+    context(`Given this friend has likes in the database`, () => {
+      beforeEach('insert friends', () =>
+        helpers.seedFriendsTables(
+          db,
+          testUsers,
+          testFriends,
+          testLikes,
+          testFriendLikes
+        )
+      );
+
+      it('Responds with 200 and the likes', () => {
+        const friendId = 1;
+        const expectedLikes = [
+          {
+            id: 1,
+            like_name: 'First Like',
+          },
+        ];
+
+        return supertest(app)
+          .get(`/api/friends/${friendId}/likes`)
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200, expectedLikes);
+      });
+    });
+
+    context(`Given an XSS attack like`, () => {
+      const testUser = helpers.makeUsersArray()[0];
+      const maliciousLike = {
+        id: 911,
+        like_name:
+          'Naughty naughty very naughty <script>alert("xss");</script>',
+      };
+      const friendLike = {
+        friend_id: testFriends[0].id,
+        like_id: maliciousLike.id,
+      };
+      const expectedLike = {
+        id: 911,
+        like_name:
+          'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
+      };
+
+      beforeEach('insert malicious like', () =>
+        helpers.seedMaliciousFriendLike(
+          db,
+          testUser,
+          testFriends[0],
+          maliciousLike,
+          friendLike
+        )
+      );
+
+      it('Removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/api/friends/${testFriends[0].id}/likes`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .expect(200)
+          .expect((res) => {
+            expect(res.body[0].like_name).to.eql(expectedLike.like_name);
+          });
+      });
+    });
+  });
+
+  describe('DELETE /api/friends/:friend_id', () => {
+    context('Given no friends', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+
+      it('Responds with 404', () => {
+        const friendId = 12345;
+        return supertest(app)
+          .get(`/api/friends/${friendId}`)
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(404, { error: `Friend doesn't exist` });
+      });
+    });
+
+    context('Given there are friends in the database', () => {
+      beforeEach('insert friends', () =>
+        helpers.seedFriendsTables(
+          db,
+          testUsers,
+          testFriends,
+          testLikes,
+          testFriendLikes
+        )
+      );
+
+      it('Responds with 204 and removes the friend', () => {
+        const friendIdToRemove = 1;
+        const testUser = testUsers[0];
+        const expectedFriends = testFriends.filter(
+          (friend) => friend.id !== friendIdToRemove
+        );
+
+        return supertest(app)
+          .delete(`/api/friends/${friendIdToRemove}`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .expect(204)
+          .then((res) => {
+            supertest(app)
+              .get(`/api/friends`)
+              .set('Authorization', helpers.makeAuthHeader(testUser))
+              .expect(expectedFriends);
+          });
+      });
+    });
+  });
+
+  describe('POST /api/friends', () => {
+    beforeEach(() => helpers.seedUsers(db, testUsers));
+    const testUser = testUsers[0];
+
+    it('creates a friend, responding with 201 and the new friend', () => {
+      const newFriend = {
+        friend_name: 'Test new friend',
+        occasion: 'Test new occasion',
+        occasion_date: '2029-01-22T00:00:00.000Z',
+        likes: [{ like_name: 'Test like' }],
+      };
+
+      return supertest(app)
+        .post(`/api/friends`)
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .send(newFriend)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.friend_name).to.eql(newFriend.friend_name);
+          expect(res.body.occasion_date).to.eql(newFriend.occasion_date);
+          expect(res.body.occasion).to.eql(newFriend.occasion);
+        })
+        .then((postRes) =>
+          supertest(app)
+            .get(`/api/friends/${postRes.body.id}`)
+            .set('Authorization', helpers.makeAuthHeader(testUser))
+            .expect((postRes) => {
+              expect(postRes.body.friend_name).to.eql(newFriend.friend_name);
+              expect(postRes.body.occasion).to.eql(newFriend.occasion);
+              expect(postRes.body.occasion_date).to.eql(
+                newFriend.occasion_date
+              );
+            })
+        );
+    });
+
+    const requiredFields = [
+      'friend_name',
+      'occasion',
+      'occasion_date',
+      'likes',
+    ];
+    requiredFields.forEach((field) => {
+      const newFriend = {
+        friend_name: 'Test new friend',
+        occasion: 'Test new occasion',
+        occasion_date: '2029-01-22T00:00:00.000Z',
+        likes: [{ like_name: 'Test like' }],
+      };
+
+      it(`responds with 400 and an error message when the ${field} is missing`, () => {
+        delete newFriend[field];
+
+        return supertest(app)
+          .post(`/api/friends`)
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .send(newFriend)
+          .expect(400, {
+            error: `Missing '${field}' in request body`,
+          });
+      });
+    });
+
+    it('Removes XSS attack content from response', () => {
+      const {
+        maliciousFriend,
+        expectedFriend,
+      } = helpers.makeMaliciousFriendWithLikes(testUsers[0]);
+      return supertest(app)
+        .post(`/api/friends`)
+        .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+        .send(maliciousFriend)
+        .then((res) => {
+          expect(res.body.friend_name).to.eql(expectedFriend.friend_name);
+          expect(res.body.occasion).to.eql(expectedFriend.occasion);
+        });
+    });
+  });
+
+  describe('PATCH /api/friends/:friend_id', () => {
+    context('Given no friends', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+
+      it('Responds with 404', () => {
+        const friendId = 12345;
+        return supertest(app)
+          .patch(`/api/friends/${friendId}`)
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(404, { error: `Friend doesn't exist` });
+      });
+    });
+
+    context('Given there are friends in the database', () => {
+      beforeEach('insert friends', () =>
+        helpers.seedFriendsTables(
+          db,
+          testUsers,
+          testFriends,
+          testLikes,
+          testFriendLikes
+        )
+      );
+
+      it('Responds with 204 and updates the friend', () => {
+        const idToUpdate = 1;
+        const testUser = testUsers[0];
+        const updateFriend = {
+          friend_name: 'updated friend name',
+          occasion: 'updated occasion',
+          occasion_date: '2000-01-22T00:00:00.000Z',
+        };
+        const expectedFriend = {
+          ...testFriends[idToUpdate - 1],
+          ...updateFriend,
+        };
+
+        return supertest(app)
+          .patch(`/api/friends/${idToUpdate}`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .send(updateFriend)
+          .expect(204)
+          .then((res) => {
+            supertest(app)
+              .get(`/api/friends/${idToUpdate}`)
+              .set('Authorization', helpers.makeAuthHeader(testUser))
+              .expect(expectedFriend);
+          });
+      });
+
+      it('Responds with 400 when no required fields supplied', () => {
+        const idToUpdate = 1;
+        const testUser = testUsers[0];
+        return supertest(app)
+          .patch(`/api/friends/${idToUpdate}`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .send({ irrelevantField: 'foo' })
+          .expect(400, {
+            error: `Request body must contain either 'friend_name', 'occasion' or 'occasion_date'`,
+          });
+      });
+
+      it('Responds with 204 when only updating a subset of fields', () => {
+        const idToUpdate = 1;
+        const testUser = testUsers[0];
+        const updateFriend = {
+          friend_name: 'updated friend name',
+        };
+        const expectedFriend = {
+          ...testFriends[idToUpdate - 1],
+          ...updateFriend,
+        };
+
+        return supertest(app)
+          .patch(`/api/friends/${idToUpdate}`)
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .send(updateFriend)
+          .expect(204)
+          .then((res) => {
+            supertest(app)
+              .get(`/api/friends/${idToUpdate}`)
+              .set('Authorization', helpers.makeAuthHeader(testUser))
+              .expect(expectedFriend);
+          });
+      });
+    });
+  });
 });
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -45,8 +45,7 @@ function makeFriendsArray(users) {
       friend_name: 'First test friend!',
       occasion: 'Birthday',
       user_id: users[0].id,
-      occasion_date: '2029-01-22',
-      likes: ['like1', 'like2', 'like3'],
+      occasion_date: '2029-01-22T00:00:00.000Z',
       date_created: '2029-01-22T16:28:32.615Z',
     },
     {
@@ -54,8 +53,7 @@ function makeFriendsArray(users) {
       friend_name: 'First test friend!',
       occasion: 'Graduation',
       user_id: users[1].id,
-      occasion_date: '2029-01-22',
-      likes: ['like1', 'like2', 'like3'],
+      occasion_date: '2029-01-22T00:00:00.000Z',
       date_created: '2029-01-22T16:28:32.615Z',
     },
     {
@@ -63,8 +61,7 @@ function makeFriendsArray(users) {
       friend_name: 'First test friend!',
       occasion: 'Anniversary',
       user_id: users[2].id,
-      occasion_date: '2029-01-22',
-      likes: ['like1', 'like2', 'like3'],
+      occasion_date: '2029-01-22T00:00:00.000Z',
       date_created: '2029-01-22T16:28:32.615Z',
     },
     {
@@ -72,55 +69,91 @@ function makeFriendsArray(users) {
       friend_name: 'First test friend!',
       occasion: 'Birthday',
       user_id: users[3].id,
-      occasion_date: '2029-01-22',
-      likes: ['like1', 'like2', 'like3'],
+      occasion_date: '2029-01-22T00:00:00.000Z',
       date_created: '2029-01-22T16:28:32.615Z',
     },
     {
       id: 5,
       friend_name: 'First test friend!',
       occasion: 'Birthday',
-      user_id: users[2].id,
-      occasion_date: '2029-01-22',
-      likes: ['like1', 'like2', 'like3'],
+      user_id: users[0].id,
+      occasion_date: '2029-01-22T00:00:00.000Z',
       date_created: '2029-01-22T16:28:32.615Z',
     },
   ];
 }
 
-function makeExpectedFriend(users, friend) {
-  const user = users.find((u) => u.id === friend.user_id);
+function makeLikesArray() {
+  return [
+    {
+      id: 1,
+      like_name: 'First Like',
+    },
+    {
+      id: 2,
+      like_name: 'Second Like',
+    },
+    {
+      id: 3,
+      like_name: 'Third Like',
+    },
+    {
+      id: 4,
+      like_name: 'Fourth Like',
+    },
+    {
+      id: 5,
+      like_name: 'Fifth Like',
+    },
+  ];
+}
 
+function makeFriendLikesArray(friends, likes) {
+  return [
+    {
+      friend_id: friends[0].id,
+      like_id: likes[0].id,
+    },
+    {
+      friend_id: friends[1].id,
+      like_id: likes[1].id,
+    },
+    {
+      friend_id: friends[2].id,
+      like_id: likes[2].id,
+    },
+    {
+      friend_id: friends[3].id,
+      like_id: likes[3].id,
+    },
+    {
+      friend_id: friends[4].id,
+      like_id: likes[4].id,
+    },
+  ];
+}
+
+function makeExpectedFriend(friend) {
   return {
     id: friend.id,
     friend_name: friend.friend_name,
     occasion: friend.occasion,
     occasion_date: friend.occasion_date,
-    date_created: friend.date_created,
-    likes: friend.likes,
-    user: {
-      id: user.id,
-      user_name: user.user_name,
-      full_name: user.full_name,
-      nickname: user.nickname,
-      date_created: user.date_created,
-    },
   };
 }
 
 function makeMaliciousFriend(user) {
   const maliciousFriend = {
     id: 911,
-    likes: ['like1', 'like2', 'like3'],
     date_created: new Date().toISOString(),
     friend_name: 'Naughty naughty very naughty <script>alert("xss");</script>',
     user_id: user.id,
-    occasion_date: '2029-01-22',
+    occasion_date: '2029-01-22T00:00:00.000Z',
     occasion:
       'Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.',
   };
   const expectedFriend = {
-    ...makeExpectedFriend([user], maliciousFriend),
+    ...makeExpectedFriend(maliciousFriend),
     friend_name:
       'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
     occasion:
@@ -132,10 +165,54 @@ function makeMaliciousFriend(user) {
   };
 }
 
+function makeMaliciousFriendWithLikes(user) {
+  const maliciousFriend = {
+    id: 911,
+    date_created: new Date().toISOString(),
+    friend_name: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    user_id: user.id,
+    occasion_date: '2029-01-22T00:00:00.000Z',
+    occasion:
+      'Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.',
+    likes: [
+      {
+        like_name: 'Test like',
+      },
+    ],
+  };
+  const expectedFriend = {
+    ...makeExpectedFriend(maliciousFriend),
+    friend_name:
+      'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
+    occasion:
+      'Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.',
+  };
+  return {
+    maliciousFriend,
+    expectedFriend,
+  };
+}
+
+function makeExpectedFriendLikes(friendId, friendLikes, likes) {
+  const expectedLikes = likes.filter(
+    (like) =>
+      like.id === friendLikes.like_id && friendLikes.friend_id === friendId
+  );
+
+  return expectedLikes.map((like) => {
+    return {
+      id: like.id,
+      like_name: like.like_name,
+    };
+  });
+}
+
 function makeFriendsFixtures() {
   const testUsers = makeUsersArray();
   const testFriends = makeFriendsArray(testUsers);
-  return { testUsers, testFriends };
+  const testLikes = makeLikesArray();
+  const testFriendLikes = makeFriendLikesArray(testFriends, testLikes);
+  return { testUsers, testFriends, testLikes, testFriendLikes };
 }
 
 function cleanTables(db) {
@@ -143,17 +220,23 @@ function cleanTables(db) {
     trx
       .raw(
         `TRUNCATE
+      friend_likes,
+      considerate_likes,
       considerate_friends,
       considerate_users`
       )
       .then(() =>
         Promise.all([
           trx.raw(
+            'ALTER SEQUENCE considerate_likes_id_seq minvalue 0 START WITH 1'
+          ),
+          trx.raw(
             'ALTER SEQUENCE considerate_friends_id_seq minvalue 0 START WITH 1'
           ),
           trx.raw(
             'ALTER SEQUENCE considerate_users_id_seq minvalue 0 START WITH 1'
           ),
+          trx.raw("SELECT setval('considerate_likes_id_seq', 0)"),
           trx.raw("SELECT setval('considerate_friends_id_seq', 0)"),
           trx.raw("SELECT setval('considerate_users_id_seq', 0)"),
         ])
@@ -177,7 +260,7 @@ function seedUsers(db, users) {
     );
 }
 
-function seedFriendsTables(db, users, friends = []) {
+function seedFriendsTables(db, users, friends, likes = [], friendLikes) {
   // use a transaction to group the queries and auto rollback on any failure
   return db.transaction(async (trx) => {
     await seedUsers(trx, users);
@@ -186,28 +269,30 @@ function seedFriendsTables(db, users, friends = []) {
     await trx.raw("SELECT setval('considerate_friends_id_seq', ?)", [
       friends[friends.length - 1].id,
     ]);
-    // only insert reviews if there are some, also update the sequence counter
-    if (reviews.length) {
-      await trx.into('considerate_reviews').insert(reviews);
-      await trx.raw("SELECT setval('considerate_reviews_id_seq', ?)", [
-        reviews[reviews.length - 1].id,
+    // only insert likes if there are some, also update the sequence counter
+    if (likes.length) {
+      await trx.into('considerate_likes').insert(likes);
+      await trx.raw("SELECT setval('considerate_likes_id_seq', ?)", [
+        likes[likes.length - 1].id,
       ]);
+      await trx.into('friend_likes').insert(friendLikes);
     }
   });
-
-  // return db
-  //   .into('considerate_users')
-  //   .insert(users)
-  //   .then(() => db.into('considerate_friends').insert(friends));
 }
 
 function seedMaliciousFriend(db, user, friend) {
-  // return db
-  //   .into('considerate_users')
-  //   .insert([user])
   return seedUsers(db, [user]).then(() =>
     db.into('considerate_friends').insert([friend])
   );
+}
+
+function seedMaliciousFriendLike(db, user, friend, like, friendLike) {
+  return db.transaction(async (trx) => {
+    await seedUsers(trx, [user]);
+    await trx.into('considerate_friends').insert([friend]);
+    await trx.into('considerate_likes').insert([like]);
+    await trx.into('friend_likes').insert([friendLike]);
+  });
 }
 
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
@@ -223,11 +308,14 @@ module.exports = {
   makeFriendsArray,
   makeExpectedFriend,
   makeMaliciousFriend,
+  makeMaliciousFriendWithLikes,
+  makeExpectedFriendLikes,
 
   makeFriendsFixtures,
   cleanTables,
   seedFriendsTables,
   seedMaliciousFriend,
+  seedMaliciousFriendLike,
   makeAuthHeader,
   seedUsers,
 };
